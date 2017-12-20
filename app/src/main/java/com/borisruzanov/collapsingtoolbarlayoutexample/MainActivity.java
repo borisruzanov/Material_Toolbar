8e6dfb75394c24dd166af4a0c294d0eed7986c43
borisruzanov <boris.ruzanov@gmail.com>
2017-12-19 22:56:07 -0500

    first setup

---
+++ app/src/main/java/com/borisruzanov/collapsingtoolbarlayoutexample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.borisruzanov.collapsingtoolbarlayoutexample;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Fab On Click Code
+
+                Snackbar.make(view, "Snack Bar Appears", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
